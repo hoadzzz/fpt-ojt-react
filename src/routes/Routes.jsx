6d5c21ebeb7b65a6acb59341aa648275eef8f927
vanhoa <levanhoa190698@gmail.com>
2022-04-25 10:29:42 +0700

    route search

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { Route, Switch } from "react-router-dom";
 
-<<<<<<< HEAD
 import Home from "../pages/Home/Home";
 import Catalog from "../pages/Catalog/Catalog";
 import Cart from "../pages/Cart/Cart";
@@ -12,17 +11,7 @@ import Login from "../pages/Log-in/Login";
 import Signup from "../pages/Sign-up/Signup";
 import Accessories from "../pages/Accessories/Accessories";
 import AccessoriesDetail from "../pages/Accessories/AccessoriesDetail";
-=======
-import Home from "../views/Home/Home";
-import Catalog from "../views/Catalog/Catalog";
-import Cart from "../views/Cart/Cart";
-import Product from "../views/Product/Product";
-import Contact from "../views/Contact/Contact";
-import Login from "../views/Log-in/Login";
-import Signup from "../views/Sign-up/Signup";
-import Accessories from "../views/Accessories/Accessories";
-import Search from "../views/Search/Search"
->>>>>>> bcacd08 (fix css header)
+import Search from "../pages/Search/Search";
 
 const Routes = () => {
   return (
@@ -35,11 +24,8 @@ const Routes = () => {
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
       <Route path="/accessories" component={Accessories}/>
-<<<<<<< HEAD
       <Route path="/accessories/:slug" component={AccessoriesDetail}/>
-=======
-      <Route path="/search" component={Search} />
->>>>>>> bcacd08 (fix css header)
+      <Route path="/search" component={Search}/>
     </Switch>
   );
 };
