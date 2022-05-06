d744af96050c23f0d4c7d7eed41b5d8918f4cf20
DuanML <maileduan24899@gmail.com>
2022-05-06 12:36:16 +0700

    fix dark theme search page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Experience from '../../components/templates/Experience/Experience'
+import Helmet from '../../components/templates/Helmet/Helmet'
 import Intro from '../../components/templates/Intro/Intro'
 
 const About = () => {
     return (
         <div className='About'>
-            <Intro />
-            <Experience/>
+            <Helmet title='About'>
+                <Intro />
+                <Experience />
+            </Helmet>
         </div>
 
     )
